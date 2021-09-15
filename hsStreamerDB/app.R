788e18e7b768f17e02529013905f4a67072661d8
Bane <mbane@ea.com>
2021-09-15 12:59:44 +1000

    Added filters and html cells to app

--- hsStreamerDB/app.R
+++ hsStreamerDB/app.R
@@ -4,16 +4,20 @@
 #
 # Find out more about building applications with Shiny here:
 #
-#    http://shiny.rstudio.com/
-#
+# http://shiny.rstudio.com
 
 library(shiny)
 library(tidyverse)
 library(DT)
+library(glue)
 
 data1 <- read_csv('data/videoMetadata.csv')
 # data2 <- read_csv('data/deckCodes.csv')
-data3 <- read_csv('data/decks.csv')
+data3 <- read_csv('data/decks.csv') %>%
+    mutate(Tribe = coalesce(Tribe, 'NONE'))
+
+classes <- c("DEMONHUNTER", "DRUID", "HUNTER", "MAGE", "PALADIN", "PRIEST",    
+             "ROGUE", "SHAMAN", "WARLOCK", "WARRIOR", 'NEUTRAL')
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -25,25 +29,29 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             
-            textInput(inputId = 'cardChoice',
-                      label = 'Contains Cards:',
-                      value = '',
-                      placeholder = 'Baku|Genn'
-                      ),
-            
             textInput(inputId = 'titleChoice',
                       label = 'Search Title:',
                       value = '',
-                      placeholder = '100% Win-Rate'
-            ),
+                      placeholder = '100% Win-Rate'),
             
             selectInput(inputId = 'classChoice',
                         label = 'Contains Class Cards:',
-                        choices = unique(data3$Class),
-                        selected = unique(data3$Class),
+                        choices = classes,
+                        selected = classes,
+                        multiple = TRUE),
+            
+            textInput(inputId = 'cardChoice',
+                      label = 'Contains Card:',
+                      value = '',
+                      placeholder = 'Baku|Genn'),
+            
+            selectInput(inputId = 'tribeChoice',
+                        label = 'Contains Tribes:',
+                        choices = sort(unique(data3$Tribe)),
+                        selected = sort(unique(data3$Tribe)),
                         multiple = TRUE)
             
-        ),
+            ),
 
         # Show a plot of the generated distribution
         mainPanel(
@@ -59,18 +67,31 @@ server <- function(input, output) {
         
         inner_join(data1, data3,
                    by = 'id') %>%
-            filter(str_detect(str_to_upper(Card), str_to_upper(input$cardChoice)),
-                   str_detect(str_to_upper(title), str_to_upper(input$titleChoice)),
-                   Class %in% input$classChoice) %>%
-            select(channel_title,
-                   title,
-                   publication_date,
-                   deckCode,
-                   url) %>%
-            mutate(deckLink = paste0('https://playhearthstone.com/en-us/deckbuilder?deckcode=',
-                                     deckCode)) %>%
+            mutate(Deck = glue('<a href="https://playhearthstone.com/en-us/deckbuilder?deckcode={deckCode}">link</a>'),
+                   Youtube = glue('<a href="{url}">link</a>'),
+                   Published = lubridate::as_date(publication_date)) %>%
+            filter(str_detect(str_to_upper(title), str_to_upper(input$titleChoice)),
+                   str_detect(str_to_upper(Card), str_to_upper(input$cardChoice)),
+                   Class %in% c(input$classChoice),
+                   Tribe %in% input$tribeChoice) %>%
+            arrange(desc(Published)) %>%
+            select(Creator = channel_title,
+                   Published,
+                   Video = title,
+                   Youtube,
+                   Deck,
+                   Code = deckCode) %>%
             distinct() %>%
-            arrange(desc(publication_date))
+            datatable(escape = FALSE,
+                      rownames = FALSE,
+                      options = list(columnDefs = list(list(
+                          targets = 5,
+                          render = JS(
+                              "function(data, type, row, meta) {",
+                              "return type === 'display' && data.length > 6 ?",
+                              "'<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;",
+                              "}")
+                      ))), callback = JS('table.page(3).draw(false);'))
         
         })
 }
